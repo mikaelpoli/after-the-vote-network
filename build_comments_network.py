3ebc47bec8c4af7ad901e3b6ca6b63c6ecc6a567
mp049 <mikaelpoli.alt@gmail.com>
2025-05-21 12:52:47 +0200

    Built comments network

--- build_comments_network.py
+++ build_comments_network.py
@@ -4,7 +4,7 @@
 
 """ SETUP """
 # LIBRARIES
-import json
+import networkx as nx
 import pandas as pd
 from pathlib import Path
 import sys
@@ -28,25 +28,35 @@ POSTS_FILTERED_CLEAN_DIR = POSTS_FILTERED_DIR / 'clean'
 COMMENTS_DIR = DATA_DIR / 'comments'
 COMMENTS_CLEAN_DIR = COMMENTS_DIR / 'clean'
 RESULTS_DIR = BASE_DIR / 'results'
+RESULTS_GRAPHS_DIR = RESULTS_DIR / 'graphs'
 
 # CUSTOM LIBRARIES
-import utils
-import preprocess as prep
+import build_network as bn
 
 
 """ IMPORT DATA """
-# POSTS
+# COMMENTS
 # Load data from JSON
-json_files = list(POSTS_FILTERED_CLEAN_DIR.glob('*.json'))
-loaded_data = {}
-for file in json_files:
-    key = Path(file).stem.replace('_clean', '')
-    with open(file, 'r', encoding='utf-8') as f:
-        loaded_data[key] = json.load(f)
+filename = COMMENTS_CLEAN_DIR / 'all_comments_clean.json'
+comments = pd.read_json(filename)
 
-# Convert to df
-posts_clean = {}
-for key, value in loaded_data.items():
-    if isinstance(value, list):
-        posts_clean[key] = pd.DataFrame(value)
-    print(f"Length posts df '{key}': {len(posts_clean[key])}")
+
+""" BUILD BIPARTITE NETWORK """
+network = bn.BuildNetwork(comments)
+network.build(tfidf=False)
+
+# Build bipartite graph
+G = bn.to_networkx_bipartite(network, use='Pwd')
+
+# Visualize or analyze
+print(f"Graph has {G.number_of_nodes()} nodes and {G.number_of_edges()} edges.")
+
+# Save to Gephi-compatible format
+filename = RESULTS_GRAPHS_DIR / 'comments.gexf'
+nx.write_gexf(G, filename)
+
+""" ANALYZE DEGREE DISTRIBUTION """
+network.plot_degree_distribution(type='words')
+network.plot_degree_distribution(type='documents')
+network.analyze_degree_distribution_powerlaw(type='words')
+network.analyze_degree_distribution_powerlaw(type='documents')
