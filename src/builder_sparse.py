44423c38904b19a46a2f04010dad8518e24d56a4
mp049 <mikaelpoli.alt@gmail.com>
2025-05-29 15:14:48 +0200

    Added support for sparse matrices

--- src/builder_sparse.py
+++ src/builder_sparse.py
@@ -4,9 +4,7 @@ from collections import defaultdict
 from pathlib import Path
 import igraph as ig
 import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
-import pandas as pd
 import pickle
 import powerlaw
 import scipy.sparse as sp
