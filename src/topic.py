f61a763bb54f6d7b9c520eda73f17a85fb397906
mp049 <mikaelpoli.alt@gmail.com>
2025-06-05 17:04:15 +0200

    Added extract_community_assignments() and plot_topic_pattern()

--- src/topic.py
+++ src/topic.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 from IPython.display import HTML
 from pathlib import Path
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, find, lil_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 import igraph as ig
 import leidenalg
@@ -76,3 +76,58 @@ def top_docs_per_topic(n_repr, df, og_text_col, topic_col, rank_col):
         top_docs_per_topic[topic] = top_docs.tolist()
 
     return top_docs_per_topic
+
+
+def extract_community_assignments(bert_model_topics, topic_ids_start=-1):
+    topic_assignments = bert_model_topics
+    num_docs = len(topic_assignments)
+    num_topics = max(topic_assignments) + topic_ids_start  # Topic IDs start from -1
+
+    C = lil_matrix((num_docs, num_topics + topic_ids_start))  # +1 to handle topic -1
+    for i in range(num_topics + 1):
+        C[np.array(topic_assignments) == (i - 1), i] = 1
+    C = C.tocsr()
+
+    # Remove zero columns
+    C = C[:, np.unique(find(C)[1])]
+
+    return num_docs, num_topics, C
+
+
+def plot_topic_pattern(num_docs, C):
+    # Order topics
+    topic_of_doc = C.argmax(axis=1).A1
+    order = np.argsort(topic_of_doc)
+
+    # Count docs per topic in ordered array:
+    _, counts = np.unique(topic_of_doc[order], return_counts=True)
+
+    # Get relative sizes
+    rel_sizes = counts / counts.sum()
+    _, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+
+    # Left plot: topic size distribution
+    ax[0].plot(np.arange(len(rel_sizes)), rel_sizes, 'o-', color='blue')
+    ax[0].set_title('BERTopic Relative cluster sizes')
+    ax[0].set_xlabel('Topic index')
+    ax[0].set_ylabel('Proportion of docs')
+    ax[0].grid(True)
+
+    # Right plot: block diagonal matrix pattern
+    cmap = plt.cm.viridis
+    ax[1].set_facecolor(cmap(0))
+    start = 0
+    for size in counts:
+        ax[1].fill_betweenx([start, start + size], start, start + size, color=cmap(1.0))
+        start += size
+
+    ax[1].set_xlim(0, num_docs)
+    ax[1].set_ylim(0, num_docs)
+    ax[1].invert_yaxis()
+    ax[1].set_aspect('equal')
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
+    ax[1].set_title('BERTopic Cluster block pattern')
+
+    plt.tight_layout()
+    plt.show()
