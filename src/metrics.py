3fee50f36d9dab7e04552167cba92ff5522c4fed
mp049 <mikaelpoli.alt@gmail.com>
2025-06-08 19:49:37 +0200

    Calculated NMI, Ncut, Modularity

--- src/metrics.py
+++ src/metrics.py
@@ -1,7 +1,8 @@
 """ SETUP """
 # LIBRARIES
 from pathlib import Path
-from sklearn.metrics.pairwise import cosine_similarity
+import scipy.sparse as sps
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import sys
 import time
@@ -28,26 +29,72 @@ RESULTS_DIR = BASE_DIR / 'results'
 
 """ FUNCTIONS """
 
-def build_adjacency_from_embeddings(embeddings, threshold=0.3):
-    sim_matrix = cosine_similarity(embeddings)
-    np.fill_diagonal(sim_matrix, 0)  # Remove self-similarity
-    A = (sim_matrix > threshold).astype(float) * sim_matrix  # Thresholding
-    return A
+def logg(x):
+    y = np.log(x)
+    y[x == 0] = 0
+
+    return y
 
 
-def compute_P_dd(A):
-    row_sums = A.sum(axis=1, keepdims=True)
-    row_sums[row_sums == 0] = 1  # prevent division by zero
-    return A / row_sums
+def build_probability_matrices(Mwd, topics):
+    Mwd = Mwd.astype(np.float64)
+
+    # Remove outlier documents
+    topics = np.array(topics)
+    valid_idx = topics != -1
+    topics = topics[valid_idx]
+    Mwd = Mwd[:, valid_idx]
+    
+    n_words, n_docs = Mwd.shape
+    n_topics = topics.max() + 1
+
+    # Document-topic matrix
+    rows = np.arange(n_docs)
+    cols = topics
+    data = np.ones(n_docs)
+    C = sps.csr_matrix((data, (rows, cols)), shape=(n_docs, n_topics))
+
+    # Compute P(w,d)
+    Pwd = Mwd / Mwd.sum()
+
+    # Compute P(w,c)
+    Pwc = Pwd @ C
+    Pwc = Pwc / Pwc.sum()
+
+    # Compute P(d,d)
+    pw = Pwd.sum(axis=1).flatten()  # p(w)
+    pw[pw == 0] = 1                 # Avoid division by 0
+    Pdd = (Pwd.T / pw) @ Pwd        # shape: (n_docs, n_docs)
+
+    # Compute P(c,c)
+    Pcc = C.T @ Pdd @ C
+
+    return Pwc, Pdd, Pcc
 
 
-def compute_P_tt(P_dd, C):
-    return C.T @ P_dd @ C
+def calculate_nmi(Pwc):
+    aw = Pwc.sum(axis=1).flatten()  # Word marginal probs
+    ac = Pwc.sum(axis=0).flatten()  # Topic marginal probs
+
+    # Avoid division by zero
+    aw[aw == 0] = 1e-12
+    ac[ac == 0] = 1e-12
+
+    Hc = np.multiply(ac, -logg(ac)).sum()  # Entropy of topics
+
+    A2 = ((Pwc / ac).T / aw).T  # Joint over marginals
+    A2.data = logg(A2.data)
+
+    return (Pwc.multiply(A2)).sum() / Hc  # NMI
 
 
-def compute_p_t(P_tt):
-    return P_tt @ np.ones((P_tt.shape[0], 1))
+def calculate_ncut(A):
+    row_sum = np.array(A.sum(axis=1)).flatten()
+    col_sum = np.array(A.sum(axis=0)).flatten()
+    cut_value = ((col_sum - A.diagonal()) / col_sum).mean()
+
+    return cut_value
 
 
-def compute_modularity(P_tt, p_t):
-    return P_tt.trace() - np.dot(p_t.flatten(), p_t.flatten())
+def calculate_modularity(Pcc):
+    return Pcc.trace() - (Pcc.sum(axis=0) * Pcc.sum(axis=1)).item()
